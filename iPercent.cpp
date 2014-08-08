00e90d0aecb2ff975e42c2c9ab24e5067b293b09
Siddhi--Jagtap <siddhi.jagtap@cumminscollege.in>
2014-08-08 08:59:14 +0530

    Update iPercent.cpp

--- iPercent.cpp
+++ iPercent.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main ()
 {
-  cout << "\nThis is 4 stars group!! ";
+  cout << "\nThis is 4 stars group!!";
   cout << "\nWhere is the 5th star??";
-  cout << "\nThis is a program for organization. ";
+  cout << "\nThis is a program for organization.";
   
   return 0;
 }
