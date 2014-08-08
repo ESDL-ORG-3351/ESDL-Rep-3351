7c1565328d8a0ad90cb4cc4b5c7b03fbc7d5b51a
Neha-Kshirsagar <neha.kshirsagar@cumminscollege.in>
2014-08-08 09:03:25 +0530

    Update iPercent.cpp

--- iPercent.cpp
+++ iPercent.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main ()
 {
-  cout << "\nThis is 4 stars group!!";
-  cout << "\nWhere is the 5th star??";
-  cout << "\nThis is a program for organization.";
+  cout << "\n This is 4 stars group!!";
+  cout << "\n Where is the 5th star??";
+  cout << "\n This is a program for organization.";
   
   return 0;
 }
