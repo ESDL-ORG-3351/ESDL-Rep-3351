cd9cdc9ad18dc7bc870f37e6a8ab059f9872d313
neha-kshirsagar <neha.kshirsagar@cumminscollege.in>
2014-09-12 09:29:26 +0530

    Added a new line.
    Updation is done by Neha & Maithi.

--- iPercent.cpp
+++ iPercent.cpp
@@ -8,6 +8,7 @@ int main ()
   cout << "\nWhere is the 5th star??";
   cout << "\nThis is a program for organization.";
   cout << "\nDone in GitHub!!";
-  
+  cout << "\nToday is Friday.";
+
   return 0;
 }
